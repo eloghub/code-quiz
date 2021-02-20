b463fe272c40bad5239dec372c6236e8c1915525
eloggit <elogtechmail@gmail.com>
2021-02-20 16:43:36 -0500

    answer choices now highlighted in blue boxes

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,10 @@ var startButton = document.getElementById("start")
 startButton.addEventListener("click", function(){
     document.getElementById("home-page").style.display = "none";
     document.getElementById("q1").style.display = "block";
-    var liQuestion = document.querySelector("li");
-    console.log(liQuestion);
-})
+    var liAnswer = document.querySelector("li");
+    console.log(liAnswer);
+});
+
+liAnswer.addEventListener("click", function(){
+    document
+});
