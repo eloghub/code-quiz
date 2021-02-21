6b424506488c6adcbd3f37bc5b67d2a901090439
eloggit <elogtechmail@gmail.com>
2021-02-20 21:00:04 -0500

    added in comments about the variables, and first 2 events (in js)

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,33 @@
-var startButton = document.getElementById("start")
+//variables listed out, starting with the "start" button,
+//including the id's for each question
+var startButton = document.getElementById("start");
 var liAnswer = document.querySelectorAll("li");
+var questionOne = document.getElementById("q1");
+var questionTwo = document.getElementById("q2");
+var questionThree = document.getElementById("q3");
+var questionFour = document.getElementById("q4");
+var questionFive = document.getElementById("q5");
 
+//when the start button is clicked, the home page disappears
+//and the first question is displayed
 startButton.addEventListener("click", function(){
     document.getElementById("home-page").style.display = "none";
-    document.getElementById("q1").style.display = "block";
+    questionOne.style.display = "block";
 });
 
+//when an answer is clicked for the first question...
 liAnswer.addEventListener("click", function(){
-    document.getElementById("q1").style.display = "none";
-    document.getElementById("q2").style.display = "block";
+    questionOne.style.display = "none";
+    questionTwo.style.display = "block";
     console.log(liAnswer);
 
 });
 
 //timer
+// setInterval(displayTime, 1000;)
 
 //score
+// incrementEl.addEventListener("click", function(){
+//     count ++;
+//     setCounterText();
+// })
