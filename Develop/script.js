30a5cc6b1fa252ca21f55d68ff6d305c2624fed8
eloggit <elogtechmail@gmail.com>
2021-03-18 18:33:07 -0400

    added functions so that clicking answer buttons pushes to the next page

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 //variables listed out, starting with the "start" button,
 //including the id's for each question, score and timer
 var startButton = document.getElementById("start");
-var liAnswer = document.querySelector("li");
+var liAnswer = document.getElementsByTagName("li");
 var questionOne = document.getElementById("q1");
 var questionTwo = document.getElementById("q2");
 var questionThree = document.getElementById("q3");
@@ -14,51 +14,97 @@ var incorrect = document.querySelectorAll("#incorrect");
 //score and timer heading
 var score = document.getElementById("score");
 var time = document.getElementById("time");
+var counter = 0
+console.log(liAnswer);
+
+function nextQuestion (){
+    console.log("I have been clicked");
+    counter++;
+    if (counter===2){
+        questionOne.style.display = "none";
+        questionTwo.style.display = "block";
+    }
+
+    else if (counter === 3){
+        questionTwo.style.display = "none";
+        questionThree.style.display = "block";
+    }
+
+    else if (counter === 4){
+        questionThree.style.display = "none";
+        questionFour.style.display = "block";
+    }
+
+    else if (counter === 5){
+        questionFour.style.display = "none";
+        questionFive.style.display = "block";
+    }
+
+    else {
+        console.log("End page");
+    }
+
+
+};
+
+for (var i=0; i<liAnswer.length; i++){
+    // console.log(liAnswer[i]);
+    liAnswer[i].addEventListener("click", function(event){
+        event.preventDefault();
+        console.log(event.target);
+        var choice = event.target;
+        console.log(choice.parentNode.className)
+        nextQuestion();
+    });
+    
+}
 
 //when the start button is clicked, the home page disappears
 //and the first question is displayed
 startButton.addEventListener("click", function(){
     document.getElementById("home-page").style.display = "none";
     questionOne.style.display = "block";
+    counter++;
 });
 
 //when an answer is clicked for the first question...
 liAnswer.addEventListener("click", function(){
-    questionOne.style.display = "none";
-    questionTwo.style.display = "block";
-    questionThree.style.display = "none";
-    questionFour.style.display = "none";
-    questionFive.style.display = "none";
-    console.log(liAnswer);
+    // questionOne.style.display = "none";
+    // questionTwo.style.display = "block";
+    // questionThree.style.display = "none";
+    // questionFour.style.display = "none";
+    // questionFive.style.display = "none";
+    nextQuestion();
+    // console.log(liAnswer);
 
 });
 
 //when an answer is clicked for the second question...
-liAnswer.addEventListener("click", function(){
-    questionTwo.style.display = "none";
-    questionThree.style.display = "block";
-    questionFour.style.display = "none";
-    questionFive.style.display = "none";
-    console.log(liAnswer);
+// liAnswer.addEventListener("click", function(){
+//     questionTwo.style.display = "none";
+//     questionThree.style.display = "block";
+//     questionFour.style.display = "none";
+//     questionFive.style.display = "none";
+//     console.log(liAnswer);
 
-});
+// });
 
 //when an answer is clicked for the third question...
-liAnswer.addEventListener("click", function(){
-    questionThree.style.display = "none";
-    questionFour.style.display = "block";
-    questionFive.style.display = "none";
-    console.log(liAnswer);
+// liAnswer.addEventListener("click", function(){
+//     questionThree.style.display = "none";
+//     questionFour.style.display = "block";
+//     questionFive.style.display = "none";
+//     console.log(liAnswer);
 
-});
+// });
 
 //when an answer is clicked for the fourth question...
-liAnswer.addEventListener("click", function(){
-    questionFour.style.display = "none";
-    questionFive.style.display = "block";
-    console.log(liAnswer);
+// liAnswer.addEventListener("click", function(){
+//     questionFour.style.display = "none";
+//     questionFive.style.display = "block";
+//     console.log(liAnswer);
 
-});
+// });
 
 //when an answer is clicked for the fifth question...
 // liAnswer.addEventListener("click", function(){
